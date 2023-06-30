4f89ffbe747bf7e06df8954abd9e939c5db51b1b
RachitBodhare <88034731+RachitBodhare@users.noreply.github.com>
2023-06-30 19:24:32 +0530

    Add files via upload

---
+++ OrganizationStats.sql
@@ -0,0 +1,11 @@
+CREATE TABLE OrganizationStats (
+    Year INT,
+    NumberOfActiveOrganizations INT
+);
+
+INSERT INTO OrganizationStats (Year, NumberOfActiveOrganizations)
+VALUES
+    (2016, 33100),
+    (2017, 1500000),
+    (2018, 2100000),
+    (2019, 2900000);
---
+++ developerstats.sql
@@ -0,0 +1,13 @@
+CREATE TABLE DeveloperStats (
+    Year INT,
+    NumberOfDevelopers FLOAT
+);
+
+INSERT INTO DeveloperStats (Year, NumberOfDevelopers)
+VALUES
+    (2016, 5.8),
+    (2017, 24),
+    (2018, 31),
+    (2019, 40),
+    (2020, 56),
+    (2022, 94);
